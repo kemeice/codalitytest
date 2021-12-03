ed4e5994dd03692692682b5ed4b1bf8349d4f877
Kemeice Parson <kem@kemstech.com>
2021-12-02 19:28:30 -0500

    Create containsdupicate.php

---
+++ containsdupicate.php
@@ -0,0 +1,31 @@
+<?php
+//Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+ 
+
+//Example 1:
+
+//Input: nums = [1,2,3,1]
+//Output: true
+//Example 2:
+
+//Input: nums = [1,2,3,4]
+//Output: false
+//Example 3:
+/
+//Input: nums = [1,1,1,3,3,4,3,2,4,2]
+//Output: true
+  function containsDuplicate($nums) {
+        $arr = array_count_values($nums);
+        sort($arr);
+        $count = count($arr);
+       for($i= 0 ; $i< $count ; $i++ )
+       {
+           if($arr[$i] > 1)
+           {
+               return true;
+           }
+       }
+        return false;
+        
+    }
