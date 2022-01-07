67abfeaaf5e13bb0dfca39ec5d6639a83b58fcc1
kemeice <kemeice@gmail.com>
2022-01-07 15:34:02 -0500

    reorganization

---
+++ Graph theory/dijkstras.py
@@ -0,0 +1,37 @@
+# source shortest path algorithm.
+
+import sys
+class Graph():
+
+    def __init__(self, vertices):
+        self.V = vertices
+        self.graph = [[0 for column in range(vertices)]
+                    for row in range(vertices)]
+
+    def printSolution(self, dist):
+        print "Vertex \tDistance from Source"
+        for node in range(self.V):
+            print node, "\t", dist[node]
+    def minDistance(self, dist, sptSet):
+       min = sys.maxint
+    for u in range(self.V):
+            if dist[u] < min and sptSet[u] == False:
+                min = dist[u]
+                min_index = u
+
+        return min_index
+     def dijkstra(self, src):
+
+        dist = [sys.maxint] * self.V
+        dist[src] = 0
+        sptSet = [False] * self.V
+
+        for cout in range(self.V):
+            x = self.minDistance(dist, sptSet)
+            sptSet[x] = True
+     for y in range(self.V):
+                if self.graph[x][y] > 0 and sptSet[y] == False and \
+                dist[y] > dist[x] + self.graph[x][y]:
+                        dist[y] = dist[x] + self.graph[x][y]
+
+        self.printSolution(dist)
